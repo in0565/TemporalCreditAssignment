9fd4a567d4e820cba559fc223902a7c83d94a0ac
YoungJuYoon <33928685+in0565@users.noreply.github.com>
2020-03-10 16:30:44 +0900

    Add files via upload

---
+++ Behavior code/total_animal_Q.m
@@ -0,0 +1,101 @@
+% % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % clc; clear all; close all;
+% % %% Load beh.file
+% % cd('E:\systemsneuro\recording\DLS\recording_Y1')
+% %     behfiles = FindFiles('beh_.mat');    
+% % %% for total session
+% %   tot_t=0; sess={}; beh_dat={};
+% % for ifile=1:size(behfiles,1);
+% %     sess=struct2cell(load(behfiles{ifile}));
+% %     beh_dat{ifile,1}(:,1)=(sess{1}{2}(:,1)+1)/2; % CR: -1,1 -->0,1 
+% %     beh_dat{ifile,1}(:,2)=(sess{1}{2}(:,2)+1)/2;
+% % %     beh_dat{ifile,1}(:,3)=sess{1}{2}(:,4); %4:RT 5:log(RT)
+% %   tot_t=tot_t+(size(beh_dat{ifile},1)-3); 
+% % end
+% % 
+% % %% total alpha, beta
+% % total=[];  %total_sess={};
+% % for i=1:100
+% % [xpar_t, like_t, exitflag_t, output_t] = fitq_Q_Y1(beh_dat);
+% % total(i,1)=like_t;
+% % total(i,2:6)=xpar_t; % beta, alpha, gamma
+% % end
+% % 
+% % find(total(:,1)==min(total(:,1)))
+% % min_tot=total(find(total(:,1)==min(total(:,1))),:);
+% % 
+% % mlik=min(total(:,1)); 
+% % 
+% % AIC=2*mlik + 2*(5)
+% % BIC=2*mlik + (5).* log(tot_t)
+% % 
+% % cd('E:\thesis\Results\alpha_beta_gamma');
+% % save(['Y1_like.mat'], 'min_tot', 'AIC','BIC','tot_t');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc; clear all; close all;
+
+% Load beh.file
+cd('E:\Data\Neural analysis')
+    behfiles = FindFiles('*_LE17.mat');    
+% for total session
+  tot_t=0; sess={}; beh_dat={};
+for ifile=1:size(behfiles,1);
+    sess=struct2cell(load(behfiles{ifile}));
+    beh_dat{ifile,1}(:,1)=(sess{1}{2}(:,1)+1)/2; % CR: -1,1 -->0,1 
+    beh_dat{ifile,1}(:,2)=(sess{1}{2}(:,2)+1)/2;
+%     beh_dat{ifile,1}(:,3)=sess{1}{2}(:,4); %4:RT(converse time~reward) 5:log(RT)
+  tot_t=tot_t+(size(beh_dat{ifile},1)-3); 
+end
+
+% total alpha, beta
+total=[];  %total_sess={};
+for i=1:100
+[xpar_t, like_t, exitflag_t, output_t] = fitq_Q_Y2(beh_dat);
+total(i,1)=like_t;
+total(i,2:6)=xpar_t; % beta, alpha, gamma
+end
+
+find(total(:,1)==min(total(:,1)))
+min_tot=total(find(total(:,1)==min(total(:,1))),:);
+
+mlik=min(total(:,1)); 
+
+AIC=2*mlik + 2*(5)
+BIC=2*mlik + (5).* log(tot_t)
+
+cd('E:\Data\DH_Results\alpha_beta_gamma_1');
+save(['LE17_like.mat'],'min_tot', 'AIC','BIC','tot_t');
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% clc; clear all; close all;
+% 
+% %% Load beh.file
+% cd('E:\systemsneuro\recording\DLS\recording_Y4')
+%     behfiles = FindFiles('beh_.mat');    
+% %% for total session
+%   tot_t=0; sess={}; beh_dat={};
+% for ifile=1:size(behfiles,1);
+%     sess=struct2cell(load(behfiles{ifile}));
+%     beh_dat{ifile,1}(:,1)=(sess{1}{2}(:,1)+1)/2; % CR: -1,1 -->0,1 
+%     beh_dat{ifile,1}(:,2)=(sess{1}{2}(:,2)+1)/2;
+% %     beh_dat{ifile,1}(:,3)=sess{1}{2}(:,4); %4:RT(converse time~reward) 5:log(RT)
+%   tot_t=tot_t+(size(beh_dat{ifile},1)-3); 
+% end
+% 
+% %% total alpha, beta
+% total=[];  %total_sess={};
+% for i=1:10
+% [xpar_t, like_t, exitflag_t, output_t] = fitq_Q_Y4(beh_dat);
+% total(i,1)=like_t;
+% total(i,2:6)=xpar_t; % beta, alpha, gamma
+% end
+% 
+% find(total(:,1)==min(total(:,1)))
+% min_tot=total(find(total(:,1)==min(total(:,1))),:);
+% 
+% mlik=min(total(:,1)); 
+% 
+% AIC=2*mlik + 2*(5)
+% BIC=2*mlik + (5).* log(tot_t)
+% 
+% cd('E:\thesis\Results\alpha_beta_gamma');
+% save(['Y4_like.mat'],'min_tot', 'AIC','BIC','tot_t');
